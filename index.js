2d123c27f7abe60c58621c8164297664d5e19af7
Faith Nzula Matei <faithnzula1230@gmail.com>
2022-08-08 13:00:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push("Ralph")
+  return name;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob")
+  return name;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+ function destructivelyRemoveFirstCat(){
+  cats.shift();
+ }
+function appendCat(name) {
+  let catsTwo = [...cats];
+  catsTwo.push("Broom");
+  return catsTwo;
+}
+function prependCat(name) {
+  let catsThree = [...cats];
+  catsThree.unshift("Arnold");
+  return catsThree;
+}
+function removeFirstCat() {
+  let catsFour = [...cats];
+  catsFour.shift();
+  return catsFour;
+}
+function removeLastCat() {
+  let catsFive = [...cats];
+  catsFive.pop();
+  return catsFive;
+}
+  
